f745291c4ee840bf98c64efa1ceea039463ab2ae
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-07-12 15:05:28 -0400

    bugfix: Adiciona props validation e verificações nos respectivos campos

--- src/components/pdfDownloadButton/pdfDownloadButton.jsx
+++ src/components/pdfDownloadButton/pdfDownloadButton.jsx
@@ -1,27 +1,47 @@
 import { jsPDF } from "jspdf";
+import PropTypes from "prop-types";
 
 const PDFdownloadButton = ({ selections, openAnswers }) => {
   const generatePDF = () => {
     const doc = new jsPDF();
     let content = "";
 
-    selections.forEach((selection, index) => {
-      content += `Pergunta ${index + 1}: ${
-        selection.label
-      }\nResposta Selecionada: ${selection.answer}\n\n`;
-    });
+    if (selections) {
+      selections.forEach((selection, index) => {
+        content += `Pergunta ${index + 1}: ${
+          selection.label
+        }\nResposta Selecionada: ${selection.answer}\n\n`;
+      });
+    }
 
-    openAnswers.forEach((openAnswer, index) => {
-      content += `Pergunta ${index + 1}: ${
-        openAnswer.question
-      }\nResposta Selecionada: ${openAnswer.answer}\n\n`;
-    });
+    if (openAnswers) {
+      openAnswers.forEach((openAnswer, index) => {
+        content += `Pergunta ${index + 1}: ${
+          openAnswer.question
+        }\nResposta Selecionada: ${openAnswer.answer}\n\n`;
+      });
+    }
 
     doc.text(content, 10, 10);
     doc.save("questionario-data-insight.pdf");
   };
 
   return <button onClick={generatePDF}>Download PDF</button>;
+};
+
+PDFdownloadButton.propTypes = {
+  selections: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      answer: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  openAnswers: PropTypes.arrayOf(
+    PropTypes.shape({
+      question: PropTypes.string.isRequired,
+      answer: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default PDFdownloadButton;
