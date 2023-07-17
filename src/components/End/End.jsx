8e1f02f1f34ad114047eec61d97301d4d9b6deec
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-07-17 10:35:12 -0400

    feat: relatorio do componente End

--- src/components/End/End.jsx
+++ src/components/End/End.jsx
@@ -3,12 +3,19 @@ import "jspdf-autotable";
 import PropTypes from "prop-types";
 import { useContext } from "react";
 import { QuizContext } from "../../context/quiz";
+import { AuthContext } from "../../context/auth";
+import "./styles.css";
 
 const End = () => {
-  const [{ selections, openAnswers }] = useContext(QuizContext);
+  const [{ selections, openAnswers, score }] = useContext(QuizContext);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const authContext = useContext(AuthContext);
+  const logout = authContext.logout;  
 
   const generatePDF = () => {
     const doc = new jsPDF();
+
+    const user = JSON.parse(localStorage.getItem("user"));
 
     let data = [];
 
@@ -30,15 +37,40 @@ const End = () => {
       });
     }
 
+    const userProblem = score <= 67
+    ? `O problema da ${user.name} é DIRETIVO`
+    : `O problema da ${user.name} é Gestão de Pessoas`;
+
+    doc.text(`Nome do Usuário: ${user.name}`, 10, 10);
+    doc.text(userProblem, 10, 20);
+
     doc.autoTable({
       head: [["Pergunta", "Resposta"]],
       body: data.map(item => [item.question, item.answer]),
+      startY: 30
     });
 
     doc.save("questionario-data-insight.pdf");
   };
 
-  return <button onClick={generatePDF}>Download PDF</button>;
+  return (
+    <div id="end-container">
+      <p>
+        {score <= 67 
+          ? `O problema da ${user.name} é DIRETIVO` 
+          : `O problema da ${user.name} é Gestão de Pessoas`
+        }
+      </p>
+      <p>
+        Clique no botão abaixo para fazer o download dos resultados do seu Quiz.
+      </p>
+      <div className="button-container">
+      <button onClick={generatePDF}>Download PDF</button>
+      <button onClick={logout} >Sair</button>
+      </div>
+      
+    </div>
+  );
 };
 
 End.propTypes = {
