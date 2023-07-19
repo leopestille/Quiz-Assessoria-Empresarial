349b2c9580ae2286af539f5463b9fa0cce17b679
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-07-19 15:04:59 -0400

    feat:testando analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import AppRoutes from "./AppRoutes";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <AppRoutes />
+      <Analytics />
     </div>
   );
 }
