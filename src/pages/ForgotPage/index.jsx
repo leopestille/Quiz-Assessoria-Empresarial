e2850f87d119a90096748c1b1401c5f14bd40b95
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-08-01 15:08:30 -0400

    mostrando mensagem ao usuario

--- src/pages/ForgotPage/index.jsx
+++ src/pages/ForgotPage/index.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 
 const ForgotPage = () => {
     const [email, setEmail] = useState("");
+    const [message, setMessage] = useState(null);
     const URL = import.meta.env.VITE_APP_API_URL;
     
     const handleForgot = async (event) => {
@@ -17,7 +18,7 @@ const ForgotPage = () => {
                 email,
               }
             );
-            console.log(response.data)
+            setMessage(response.data);            
         } catch (error) {
             if (error.response) {
                 console.log(error.responde.data);
@@ -33,31 +34,35 @@ const ForgotPage = () => {
     };
     
     return (
-        <div id="forgot__form">
+      <div id="forgot__form">
         <form className="form" onSubmit={handleForgot}>
-            <h1 className="forgot">Esqueceu sua senha?</h1>
-            <h2>Digite seu email abaixo para receber o link de redefinição de senha</h2>
-            <div className="field">
+          <h1 className="forgot">Esqueceu sua senha?</h1>
+          <h2>
+            Digite seu email abaixo para receber o link de redefinição de senha
+          </h2>
+          <div className="field">
             <label htmlFor="email">Email:</label>
             <input
-                type="email"
-                name="email"
-                id="email"
-                value={email}
-                onChange={(b) => setEmail(b.target.value)}
-                required
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(b) => setEmail(b.target.value)}
+              required
             />
-            </div>
-            <div className="link-container">
+          </div>
+          <div className="link-container">
+            {/* Mostre a mensagem de retorno aqui, se ela existir */}
+            {message && <p className="response-message">{message}</p>}
             <Link to="/login">
-                <p>Voltar a área de Login</p>
+              <p>Voltar a área de Login</p>
             </Link>
             <button type="submit" className="btn">
-                Enviar
+              Enviar
             </button>
-            </div>
+          </div>
         </form>
-        </div>
+      </div>
     );
 }
 
