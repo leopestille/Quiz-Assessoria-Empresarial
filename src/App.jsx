f5c3151c1f074df3d786d3e9fdca3b9feb8fb144
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-08-07 09:13:11 -0400

    incluindo analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import AppRoutes from "./AppRoutes";
+import { Analytics } from "@vercel/analytics/react";
 
 
 function App() {
   return (
     <div className="App">
       <AppRoutes />
-     
+      <Analytics />
     </div>
   );
 }
