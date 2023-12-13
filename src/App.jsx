300eabd80c6194e9ce78db06adb159ad072068fb
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-12-13 15:04:00 -0400

    "Envolveu o componente AppRoutes com ErrorBoundary para tratamento de erros"

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import "./App.css";
 import AppRoutes from "./AppRoutes";
 import { Analytics } from "@vercel/analytics/react";
+import ErrorBoundary from "./components/Error/ErrorBoundary";
 
 
-/**
-  * A função App retorna um elemento JSX que renderiza o componente AppRoutes e o componente Analytics
-  * dentro de um div com o className "App".
-  * @returns O componente App está sendo retornado. Ele contém um div com o className "App" e dois
-  * componentes filhos - AppRoutes e Analytics.
-  */
 function App() {
   return (
     <div className="App">
-      <AppRoutes />      
+      <ErrorBoundary>
+      <AppRoutes />
+      </ErrorBoundary>
     </div>
   );
 }
