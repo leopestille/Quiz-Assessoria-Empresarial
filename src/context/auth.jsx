540b4fc8d69eb11da9056867a088415376100b73
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-12-15 09:32:12 -0400

    retirando comentários

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -5,11 +5,7 @@ import PropTypes from "prop-types";
 
 export const AuthContext = createContext();
 
-/**
-  * O componente `AuthProvider` é um componente React que fornece funcionalidade de autenticação,
-  * incluindo login, logout e registro, para seus componentes filhos.
-  * @returns O componente AuthProvider está sendo retornado.
-  */
+
 export const AuthProvider = ({ children }) => {
 	const navigate = useNavigate();
 	const [user, setUser] = useState(null);
@@ -29,19 +25,12 @@ export const AuthProvider = ({ children }) => {
 		navigate("/quiz");
 	}, []);
 
-	/**
-	 * A função `clearMessageError` limpa a mensagem de erro.
-	 */
+
 	const clearMessageError = () => {
 		setErrorMessage("");
 	};
 
-	/**
-	 * A função `login` é uma função assíncrona que tenta criar uma sessão enviando um
-	 * e-mail e senha para o servidor e, se for bem-sucedido, armazena o usuário e o token no armazenamento local
-	 * , define o cabeçalho de autorização para futuras solicitações de API, define o estado do usuário e navega
-	 * para a página "/quiz". Se houver um erro, ele define o estado da mensagem de erro.
-	 */
+
 	const login = async (email, password) => {
 		try {
 			const response = await createSession(email, password);
@@ -63,10 +52,7 @@ export const AuthProvider = ({ children }) => {
 		}
 	};
 
-	/**
-	 * A função `logout` remove os dados do usuário e do token do armazenamento local, limpa a autorização
-	 * cabeçalho, define o estado do usuário como nulo e navega para a página de login.
-	 */
+
 	const logout = () => {
 		localStorage.removeItem("user");
 		localStorage.removeItem("token");
@@ -77,11 +63,7 @@ export const AuthProvider = ({ children }) => {
 		navigate("/login");
 	};
 
-	/**
-	 * A função `register` é uma função assíncrona que recebe um nome, e-mail e senha como
-	 * parâmetros e tentativas de criar um usuário com essas credenciais.
-	 * @returns A função `register` está retornando a resposta da função `createUser`.
-	 */
+
 	const register = async (name, email, password) => {
 		try {
 			const response = await createUser(name, email, password);
