a78d8ba24751a282690b9af26ef414506c0e700a
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-12-18 09:23:08 -0400

    retirando comentários

--- src/pages/RedirectLogin/index.jsx
+++ src/pages/RedirectLogin/index.jsx
@@ -1,11 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
-/**
-  * O componente RedirectLogin redireciona o usuário para a página de login se o local atual for a raiz
-  * caminho ("/").
-  * @retorna null
-  */
 const RedirectLogin = () => {
   const navigate = useNavigate();
   const location = useLocation();
